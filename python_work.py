156e017be6667ad17e7cc2241c640b6c5c869ccb
Jason Cao <jason.cao.2003@berkeley.edu>
2026-03-15 03:19:46 -0700

    q1,q3 edits

--- python_work.py
+++ python_work.py
@@ -68,3 +68,45 @@ Q2 Code
 """
 Q3 Code
 """
+
+
+
+
+
+
+
+
+
+"""Attempting to Find Confounders"""
+
+# List of all potential confounders
+controls = ['id','deg', 'yrdeg', 'field', 'startyr', 'year', 'rank', 'admin']
+
+# 1. The "Gold Standard" Full Model
+full_formula = 'salary ~ sex + ' + ' + '.join(controls)
+m_full = smf.ols(full_formula, data=df).fit(cov_type='HC3')
+
+# Get the 'sex' coefficient name (e.g., 'sex[T.Male]')
+sex_param = [c for c in m_full.params.index if 'sex' in c][0]
+full_beta = m_full.params[sex_param]
+
+# 2. Iterate and "Leave One Out"
+ovb_results = []
+
+for var in controls:
+    # Build formula excluding only the current variable
+    reduced_controls = [c for c in controls if c != var]
+    reduced_formula = 'salary ~ sex + ' + ' + '.join(reduced_controls)
+    
+    m_reduced = smf.ols(reduced_formula, data=df).fit(cov_type='HC3')
+    reduced_beta = m_reduced.params[sex_param]
+    
+    # Calculate the "Bias" that this variable was correcting
+    # If delta is large, this variable was a major confounder
+    delta = full_beta - reduced_beta
+    percent_impact = (delta / reduced_beta) * 100
+    
+    ovb_results.append({'Omitted Variable': var, 'Sex Beta (Without it)': reduced_beta, 'Shift (%)': percent_impact})
+
+ovb_df = pd.DataFrame(ovb_results).sort_values(by='Shift (%)', ascending=False)
+print(ovb_df)
